659aca4029c5f8ef2fb83ccf58c706724bce1ffc
horiuchi67 <1234mhoriuchi@gmail.com>
2020-06-03 16:31:52 +0900

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   get 'home/about' => 'home#about', as: 'about'
   root "home#top"
   devise_for :users
-  
-  resources :books, only: [:new, :create, :index, :show, :edit, :update]
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :update] do
+  	resources :book_comments, only: [:create, :destroy]
+  	resource :favorites, only: [:create, :destroy]
+  end
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :users, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
