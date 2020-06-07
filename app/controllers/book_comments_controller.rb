ab84d6288cb13061fb5c71b52c87ceb858db9d78
horiuchi67 <1234mhoriuchi@gmail.com>
2020-06-07 20:09:38 +0900

    Initial commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,15 @@
 class BookCommentsController < ApplicationController
 	def create
-	    book = Book.find(params[:book_id])
-	    comment = current_user.book_comments.new(book_comment_params)
-	    comment.book_id = book.id
-	    comment.save
-	    redirect_to request.referer
+	    @book1 = Book.find(params[:book_id])
+	    book_comment = current_user.book_comments.build(book_comment_params)
+	    book_comment.book_id = @book1.id
+	    book_comment.save
 	end
 	def destroy
+		@book1 = Book.find(params[:book_id])
 	    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-	    redirect_to request.referer
 	end
+
 	private
 	def book_comment_params
 	    params.require(:book_comment).permit(:comment)
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,12 @@
 class FavoritesController < ApplicationController
 	def create
-		book = Book.find(params[:book_id])
-		favorite = current_user.favorites.new(book_id: book.id)
+		@book1 = Book.find(params[:book_id])
+		favorite = current_user.favorites.build(book_id: @book1.id)
 		favorite.save
-		redirect_to request.referer
 	end
 	def destroy
-		book = Book.find(params[:book_id])
-		favorite = current_user.favorites.find_by(book_id: book.id)
+		@book1 = Book.find(params[:book_id])
+		favorite = current_user.favorites.find_by(book_id: @book1.id)
 		favorite.destroy
-		redirect_to request.referer
 	end
 end
